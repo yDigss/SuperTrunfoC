c6ac86073a405fd713cf0d15b7d6f7767259067c
Rodrigo Silva <rodrigo.7762018@gmail.com>
2025-08-15 20:16:31 -0300

    uso de whiles e do while

---
+++ whileExample.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+    // Uso de do while
+    
+   int i = 1;
+
+   do {
+
+    printf("%d\n", i);
+    i++;
+
+   } while (i <= 5);
+   
+   
+   
+   
+   
+   
+   
+    /*
+    Uso de while
+    int i = 1;
+
+    while (i <= 5)
+    {
+        printf("%d\n", i);
+        i++;
+    }*/
+    
+
+    return 0;
+}
